e6e14ad4ebd680838f23b08a762ed727157126cb
Lyra Von Rose <lyravonrose@gmail.com>
2021-11-05 13:53:04 +0100

    friday test

---
+++ do_things.js
@@ -0,0 +1,23 @@
+var x;
+var doubleX;
+
+x = 22;
+
+function timesTwo(num) {
+    return num * 2;
+}
+
+timesTwo(x) = doubleX;
+
+var numbers;
+
+numbers = [x, doubleX];
+
+for (var i = 0; numbers.length > i; i++);
+
+console.log(numbers[i]);
+
+numbers = {};
+
+doubleX = numbers[y];
+
