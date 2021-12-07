d00551b585f5ccbc65f39b391177b714bac838fe
Lyra Von Rose <lyravonrose@gmail.com>
2021-12-07 17:42:33 +0100

    Twitter API updated

--- twitter-ticker/index.js
+++ twitter-ticker/index.js
@@ -4,30 +4,67 @@ const { getToken, getTweets, filterTweets } = require("./twitter");
 const app = express();
 
 app.use(express.static("./ticker"));
-app.get("/ticker.json", (req, res) => {
-    console.log("client requested ticker.json");
-    // ultimately there are 4 things now, that we want to do
-    //1. get the token. when we have the token
-    getToken((err, bearerToken) => {
-        // console.log("token in getToken() index.js:", bearerToken);
-        if (err) {
-            //sth went wrong with obtaining the token
-            return res.sendStatus(500);
-        }
-        //2. make a request for the tweets using the token. When we have the tweets
-        getTweets(bearerToken, (err, tweets) => {
-            if (err) {
-                //sth went wrong with getting of tweets
-                console.log("err in getTweets:", err);
-                return res.sendStatus(500);
-            }
-            //if we make it here, we successfuly obtained tweets
-            //3. filter the tweets from step 2
-            const filteredTweets = filterTweets(tweets);
-            //filteredTweets should look like our data.json file that contained the hard coded headlines
-            //4. send back the filtered tweets(they should look the way you data.json is formatted right now)
-            res.json(filteredTweets);
+app.get("/", (req, res) => {
+    getToken()
+        .then((token) => {
+            //     const tweetPromises = [getTweets(token, "bbcworld"),
+            // getTweets(token, "nytimes"),
+            // getTweets(token, "newyorker")];
+            // return Promise.all(tweetPromises);
+
+            return Promise.all([
+                getTweets(token, "bbcworld"),
+                getTweets(token, "nytimes"),
+                getTweets(token, "newyorker"),
+            ]);
+        })
+        .then((allTheTweets) => {
+            console.log("allTheTweets", allTheTweets);
+            const bbcTweets = allTheTweets[0];
+            const nytimesTweets = allTheTweets[1];
+            const newyorkerTweets = allTheTweets[2];
+
+            const mergedTweets = [
+                ...bbcTweets,
+                ...nytimesTweets,
+                ...newyorkerTweets,
+            ];
+            //sort our tweets in reverse chronogical order
+            mergedTweets.sort((a, b) => {
+                return new Date(b.created_at) - new Date(a.created_at);
+            });
+            const tweetsFiltered = filterTweets(mergedTweets);
+            res.json(tweetsFiltered);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
         });
-    });
 });
+// app.get("/ticker.json", (req, res) => {
+//     console.log("client requested ticker.json");
+//     // ultimately there are 4 things now, that we want to do
+//     //1. get the token. when we have the token
+//     getToken((err, bearerToken) => {
+//         // console.log("token in getToken() index.js:", bearerToken);
+//         if (err) {
+//             //sth went wrong with obtaining the token
+//             return res.sendStatus(500);
+//         }
+//         //2. make a request for the tweets using the token. When we have the tweets
+//         getTweets(bearerToken, (err, tweets) => {
+//             if (err) {
+//                 //sth went wrong with getting of tweets
+//                 console.log("err in getTweets:", err);
+//                 return res.sendStatus(500);
+//             }
+//             //if we make it here, we successfuly obtained tweets
+//             //3. filter the tweets from step 2
+//             const filteredTweets = filterTweets(tweets);
+//             //filteredTweets should look like our data.json file that contained the hard coded headlines
+//             //4. send back the filtered tweets(they should look the way you data.json is formatted right now)
+//             res.json(filteredTweets);
+//         });
+//     });
+// });
 app.listen(8080, console.log("Twitter API project running 🌭"));
--- twitter-ticker/twitter.js
+++ twitter-ticker/twitter.js
@@ -6,72 +6,109 @@ console.log("secrets", secrets);
 
 const https = require("https");
 
-module.exports.getToken = function getToken(callbackGetToken) {
+module.exports.getToken = function getToken() {
     //this function will obtain a so called bearerToken from Twitter
     // here are my credentials
-    const creds = `${secrets.Key}:${secrets.Secret}`;
-    console.log("creds:", creds);
-    // let's encode our credentials so that we can send them along
-    // twitter wants them to be base64 encoded
-    const encodedCreds = Buffer.from(creds).toString("base64");
+    return new Promise((resolve, reject) => {
+        const creds = `${secrets.Key}:${secrets.Secret}`;
+        console.log("creds:", creds);
+        // let's encode our credentials so that we can send them along
+        // twitter wants them to be base64 encoded
+        const encodedCreds = Buffer.from(creds).toString("base64");
 
-    console.log("need to go get myself a token");
-    const options = {
-        host: "api.twitter.com",
-        path: "/oauth2/token",
-        method: "POST",
-        headers: {
-            Authorization: `Basic ${encodedCreds}`,
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-        },
-    };
-    //this is the function that will run when twitter API answers our request!
-    function reqCallback(response) {
-        if (response.statusCode != 200) {
-            //this means sth went wrong with our authentication to the twitter API
-            callbackGetToken(response.statusCode);
-            console.log("response.statusCode", response.statusCode);
-        }
-        // if we make it here we have a token to obtain
-        let body = "",
-        response.on("data", (chunk) => {
-            body += chunk;
-        });
-        
-        response.on("end",()=>{
-const parsedBody= JSON.parse(body);
-callbackGetToken(null, parsedBody.access_token);
-        });
-    }
-    const req = https.request(options, reqCallback);
-    req.end("grant_type=client_credentials");
-};
-module.exports.getTweets = function getTweets(bearerToken, callbackGetTweets) {
-    // this function gets the tweets from Twitter API
-    //this will be for you to compelete
-    //Make the request to get tweets GET statuses/user_timeline
+        console.log("need to go get myself a token");
         const options = {
-        host: "api.twitter.com",
-        path: "/1.1/statuses/user_timeline.json?screen_name=TheOnion&tweet_mode=extended",
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${bearerToken}`
-        }
-    };
+            host: "api.twitter.com",
+            path: "/oauth2/token",
+            method: "POST",
+            headers: {
+                Authorization: `Basic ${encodedCreds}`,
+                "Content-Type":
+                    "application/x-www-form-urlencoded;charset=UTF-8",
+            },
+        };
 
-// There must be no content type header
-// The response you will receive, will be an array of ginormous objects (each representing one tweet and a whole lot of information about said tweet!)
-// The full_text property of these objects, contains the text value of the tweet
-// Each tweet object has a property named entities. It's an object that has a property named urls. That's an array of objects representing each url in the tweet
-    console.log(`getTweets() running:
-    we have the token: ${bearerToken}
-    time to get some tweets`);
+        //this is the function that will run when twitter API answers our request!
+        function reqCallback(response) {
+            if (response.statusCode != 200) {
+                //this means sth went wrong with our authentication to the twitter API
+                reject(response.statusCode);
+                // console.log("response.statusCode", response.statusCode);
+            }
+
+            // if we make it here we have a token to obtain
+            let body = "";
+            response.on("data", (chunk) => {
+                body += chunk;
+            });
+
+            response.on("end", () => {
+                const parsedBody = JSON.parse(body);
+                resolve(parsedBody.access_token);
+            });
+        }
+        const req = https.request(options, reqCallback);
+        req.end("grant_type=client_credentials");
+    });
 };
-module.exports.filterTweets = function filterTweets(tweets) {
+
+module.exports.getTweets = function (bearerToken, tweetSource) {
+    return new Promise((resolve, reject) => {
+        // this function gets the tweets from Twitter API
+        // Make the request to get tweets GET statuses/user_timeline
+        console.log(`getTweets() running:
+    we have the token: ${bearerToken}
+    time to get some tweets ${tweetSource}`);
+
+        const options = {
+            host: "api.twitter.com",
+            path: `/1.1/statuses/user_timeline.json?screen_name=${tweetSource}&tweet_mode=extended`,
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${bearerToken}`,
+            },
+        };
+        function reqCallback(response) {
+            if (response.statusCode != 200) {
+                //this means sth went wrong with our authentication to the twitter API
+                reject(response.statusCode);
+                // console.log("response.statusCode", response.statusCode);
+            }
+            // if we make it here we have a token to obtain
+            let body = "";
+            response.on("data", (chunk) => {
+                body += chunk;
+            });
+
+            response.on("end", () => {
+                const parsedBody = JSON.parse(body);
+                resolve(parsedBody);
+            });
+        }
+        const req = https.request(options, reqCallback);
+        req.end();
+    });
+};
+
+module.exports.filterTweets = function (tweets) {
     // this function will filter/clean up the response we received from Twitter
     // and narrows down that data to what our client side actually needs (i.e. only an href and the linktext!)
-    //this is also for you to complete
     console.log(`filterTweets() running:
     we have the tweets: ${tweets}
     time to format this data`);
+    let result = [];
+
+    for (var i = 0; i < tweets.length; i++) {
+        if (tweets[i].entities.urls.length === 1) {
+            let tweet = {
+                text: tweets[i].full_text.replace(
+                    tweets[i].entities.urls[0].url
+                ),
+                url: tweets[i].entities.urls[0].url,
+            };
+            result.push(tweet);
+        }
+    }
+    console.log(result);
+    return result;
 };
